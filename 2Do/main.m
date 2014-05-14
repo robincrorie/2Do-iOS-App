d87638986bec29a648335da0b4ac34d7a3b87c1d
Robin Crorie <rcrorie@webxr.co.uk>
2014-05-14 15:35:25 +0100

    Initial Commit

---
+++ 2Do/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2Do
+//
+//  Created by Robin Crorie on 14/05/2014.
+//  Copyright (c) 2014 Robin Crorie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
