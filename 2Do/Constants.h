9f3bc4e09691de96a5427b75a46012500a402c74
Robin Crorie <rcrorie@webxr.co.uk>
2014-05-19 09:22:34 +0100

    Added a constants header file to store the urls for the web service. Added the loading screen helper class to overlay the loading symbol.

---
+++ 2Do/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  2Do
+//
+//  Created by Robin Crorie on 18/05/2014.
+//  Copyright (c) 2014 Robin Crorie. All rights reserved.
+//
+
+#ifndef _Do_Constants_h
+#define _Do_Constants_h
+#endif
+
+#define SoapURL_UserAccountManager		@"http://2do.webxr.co.uk/2Do/UserAccountManager"
+#define SoapURL_TaskManager				@"http://2do.webxr.co.uk/2Do/TaskManager"
